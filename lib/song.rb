d991b655e69ccf9d936168c4d5ee4c8b6e0c1cdf
Ingrid Wong <yw340@georgetown.edu>
2018-01-16 22:33:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,15 @@
 class Song
   @@genres = []
   @@artists = []
+  @@count = 0
   attr_accessor :name, :artist, :genre
   def initialize
+    @@count ++
     @@artists << artist
     @@genres << genre
   end
+
+  def self.count
 
   def self.genre_count
     @@genres.count
