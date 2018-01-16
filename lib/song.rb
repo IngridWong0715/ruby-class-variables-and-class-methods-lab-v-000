61f38a3f964959927df15ddaf845f6ab8d3aa92d
Ingrid Wong <yw340@georgetown.edu>
2018-01-16 22:38:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,8 @@ class Song
     @genre = genre
 
     @@count += 1
-  #  @@artists << artist if !@@artists.include?(artist)
-    #@@genres << genre if !@@genres.include?(genre)
+    @@artists << artist if !@@artists.include?(artist)
+    @@genres << genre if !@@genres.include?(genre)
   end
 
   def self.count
