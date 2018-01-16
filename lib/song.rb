7a70bf74965114c3bbab9b58aaf7be6b6e47411e
Ingrid Wong <yw340@georgetown.edu>
2018-01-16 22:37:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,11 @@ class Song
   @@artists = []
   @@count = 0
   attr_accessor :name, :artist, :genre
-  def initialize
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
     @@count ++
     @@artists << artist if !@@artists.include?(artist)
     @@genres << genre if !@@genres.include?(genre)
